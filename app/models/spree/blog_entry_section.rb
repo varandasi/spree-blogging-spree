235f777aef3803da09641daef3ecb807ca5508fe
Aaron Z Davis <aaronzdavis@gmail.com>
2016-04-04 13:36:22 -0700

    Collapse to sort sections and sort products, images
    * Update sections to handle collapsed sorting
    
    * add sorting for images

--- app/models/spree/blog_entry_section.rb
+++ app/models/spree/blog_entry_section.rb
@@ -5,9 +5,9 @@ class Spree::BlogEntrySection < ActiveRecord::Base
   acts_as_list scope: :blog_entry
 
   has_many :blog_entry_section_images, -> { order(position: :asc) }, :as => :viewable, :dependent => :destroy, :class_name => 'Spree::BlogEntryImage'
-  accepts_nested_attributes_for :blog_entry_section_images, :reject_if => :all_blank
+  accepts_nested_attributes_for :blog_entry_section_images, :reject_if => :all_blank, :allow_destroy => true
 
   has_many :vae_products, -> { order(position: :asc) }, :dependent => :destroy
-  accepts_nested_attributes_for :vae_products, :reject_if => :all_blank
+  accepts_nested_attributes_for :vae_products, :reject_if => :all_blank, :allow_destroy => true
 
 end
